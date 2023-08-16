b5069f5c309ba36660d8cbd854616ba137594812
odarley barrios <odarleybarrios@gmail.com>
2023-08-15 22:31:55 -0400

    adiciona relacionamento

--- app/Models/Automovel.php
+++ app/Models/Automovel.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Automovel extends Model
 {
-   protected $table = "automoveis"; 
+   protected $table = "automoveis";
+   public function cliente(){
+
+      return $this->belongsTo(cliente::class,'cliente_id','id');
+   }
+   
 }
--- app/Models/Ocorrencia.php
+++ app/Models/Ocorrencia.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ocorrencia extends Model
 {
-  protected $table = "ocorrencias";  
+
+  protected $table = "ocorrencias"; 
+  public function automovel()
+  {
+    return $this->belongsTo(automovel::class,'automovel_id','id');
+  }
+  
 }
